d9918acbdc2a00fc6e44c14dfe233392a6455deb
Stefan <simanek.s@gmx.at>
2026-09-10 16:45:35 +0200

    wip

---
+++ PlaywrightBasics/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "SYSLIB1045:Convert to 'GeneratedRegexAttribute'.", Justification = "<Pending>", Scope = "member", Target = "~M:PlaywrightBasics.Tests.Lesson01_FirstTest.Navigate_And_Check_The_Title~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Performance", "SYSLIB1045:Convert to 'GeneratedRegexAttribute'.", Justification = "<Pending>", Scope = "member", Target = "~M:PlaywrightBasics.Tests.Lesson01_FirstTest.Click_A_Link_And_Check_Where_We_Landed~System.Threading.Tasks.Task")]
--- PlaywrightBasics/Tests/Lesson01_FirstTest.cs
+++ PlaywrightBasics/Tests/Lesson01_FirstTest.cs
@@ -2,7 +2,7 @@ using System.Text.RegularExpressions;
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 
-namespace PlaywrightBasics.Basics;
+namespace PlaywrightBasics.Tests;
 
 /// <summary>
 /// LESSON 1 — The shape of a Playwright test.
